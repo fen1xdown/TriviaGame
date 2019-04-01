f4a35eb56e7f9a681fd75fd4470d951e531f418e
cameron calder <cameronleecalder@gmail.com>
2019-03-31 20:42:10 -0500

    204231032019 commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+// start with multiple variables to hold the questions and answers for the trivia game (most likely arrays within a master array)
+// create a variable to hold the users choice for the question
+// add more variables, to count correct and incorrect guesses, and a last variable to show the users score (out of 100)
+
+// create logic to get questions at random and their answers and display them using the HTML
+    // example = var random = Math.floor(Math.random() * 1000) + 1; <=== something like that
+
+// create a timer, display it to the user, and if the timer reaches 0 before the user is done, stop the quiz and count all questions not answered as wrong
+    // ex: setTimeout(timeUp, 15000);
+
+// use buttons in html for the multiple choice questions and log the users response
+    // ex: newDiv.button("option A"); etc
+
+// create logic to determind if the users choices are right or wrong
+    // if (userAnswer === correctAnswer) {....} else {......}
+// take the questions that the user got correct and turn it into a score out of 100 possible points.
+    // questionsRight.value * x (and put it over 100)
+// create a button to reset the game without having to refresh the page.
